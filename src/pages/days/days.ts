a41e27224b13eaedea964761d99ed72e39879fe1
Felix Gellner <flink@flinkblog.de>
2017-11-22 18:11:10 +0100

    Simple Pagination working!

--- src/pages/days/days.ts
+++ src/pages/days/days.ts
@@ -1,8 +1,8 @@
 import { DayProvider } from './../../providers/days/day';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Day } from '../../models/days/day';
 import { ModalsPage } from '../modals/modals';
-import { IonicPage, NavController, Platform, ModalController } from 'ionic-angular';
+import { IonicPage, NavController, Platform, ModalController, Content } from 'ionic-angular';
 import { ImageProvider } from '../../providers/image/image';
 import { PreloaderProvider } from '../../providers/preloader/preloader';
 import { DatabaseProvider } from '../../providers/data/database';
@@ -24,27 +24,37 @@ import 'rxjs/Rx';
 })
 export class DaysPage {
 
+  @ViewChild(Content) content: Content;
+
   @Input() day : Day;
 
-  private auth     : any;
-  public days      : any;
-  private email    : string = 'mail@mail.com';
-  private pass     : string = 'password';
+  private auth            : any;
+  public days             : any;
+  public daysOfMonth      : any;
+  private email           : string = 'mail@mail.com';
+  private pass            : string = 'password';
+
+  public current_date = new Date();
+  public date_of_page = new Date();
+  public current_month = this.current_date.getMonth();
+  public current_year = this.current_date.getFullYear();
 
   constructor(
-    private dayProvider  : DayProvider,
-    public navCtrl       : NavController,
-    private platform     : Platform,
-    private modalCtrl    : ModalController,
-    private _IMG         : ImageProvider,
-    private _LOADER      : PreloaderProvider,
-    private _DB          : DatabaseProvider)
+    private dayProvider   : DayProvider,
+    public navCtrl        : NavController,
+    private platform      : Platform,
+    private modalCtrl     : ModalController,
+    private _IMG          : ImageProvider,
+    private _LOADER       : PreloaderProvider,
+    private _DB           : DatabaseProvider)
     {
       console.log('Hello DayComponent Component');
       console.log('dayInfo: ' + this.day);
   }
 
   ionViewDidEnter(){
+    console.log("hä" + this.current_date + " " + this.current_month + " " + this.current_year);
+
      this._LOADER.displayPreloader();
     //  this.platform.ready()
     //  .then(() =>
@@ -69,12 +79,13 @@ export class DaysPage {
     let subscription = this._DB.renderDays().subscribe((data : Day[]) => {
 
         console.log("Subscription successfully got the days from the DB.");
-
         console.log(data);
 
+        //newest days first
         this.days = data.reverse();
 
-        console.log(this.days);
+        //get days of current month
+        this.daysOfMonth = this.getDaysOfMonth(this.days, this.current_month, this.current_year);
 
         // success
       }, (errData) => {
@@ -89,17 +100,53 @@ export class DaysPage {
      this._LOADER.hidePreloader();
   }
 
-  orderByDescendingDate(data: Day[]){
-    console.log("reverseDays");
-    for(let i = 0; i < data.length -1 ; i++){
-      if (data[i].date > data[i+1].date){
-        console.log(data[i].date +" war groesser als " + data[i+1].date);
+  /**
+   * Returns an array of the days which are in the specified month
+   * @param days
+   * @param month
+   */
+  getDaysOfMonth(days : Day[], filter_month, filter_year){
+    console.log("getDaysOfMonth");
+    let new_days_array = [];
+    for(let i = 0; i < days.length; i++){
+      let year = new Date(days[i].date).getFullYear();
+      let month = new Date(days[i].date).getMonth();
+      if(month == filter_month && year == filter_year){
+        // console.log("Correct Month and year: " + month +"=="+ filter_month + "; " + year + "==" + filter_year);
+        new_days_array.push(days[i]);
       }else{
-        console.log(data[i].date +" war kleiner als " + data[i+1].date);
+        // console.log("Different Month and year: " + month +"!=="+ filter_month + "; " + year + "!==" + filter_year);
       }
-
     }
-    return data;
+    return new_days_array;
+  }
+
+  previousMonth(){
+    if(this.current_month != 0){
+      this.current_month--;
+    this.daysOfMonth = this.getDaysOfMonth(this.days, this.current_month, this.current_year);
+    }else{
+      this.current_year--;
+      this.current_month = 11;
+      this.daysOfMonth = this.getDaysOfMonth(this.days, this.current_month, this.current_year);
+    }
+
+    this.date_of_page = new Date(this.current_year, this.current_month);
+    this.content.scrollToTop();
+  }
+
+  nextMonth(){
+    if(this.current_month != 11){
+      this.current_month++;
+    this.daysOfMonth = this.getDaysOfMonth(this.days, this.current_month, this.current_year);
+    }else{
+      this.current_year++;
+      this.current_month = 0;
+      this.daysOfMonth = this.getDaysOfMonth(this.days, this.current_month, this.current_year);
+    }
+
+    this.date_of_page = new Date(this.current_year, this.current_month);
+    this.content.scrollToTop();
   }
 
   /**
